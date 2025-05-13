a29a77c6fe1a54997f017eb23c2e364b758e22e4
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-05-13 12:43:41 -0300

    preparando para acesso no backend

---
+++ src/main/java/br/com/marcilioaguiar/front_gestao_vagas/modules/candidate/dto/Token.java
@@ -0,0 +1,17 @@
+package br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.dto;
+
+public class Token {
+
+    public String access_token;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    
+    
+}

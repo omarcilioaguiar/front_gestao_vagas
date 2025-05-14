206d8dd88e9a5c8a39d1f80223d8c57c43b5971a
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-05-14 12:10:40 -0300

    Implementação de Autenticação

--- src/main/java/br/com/marcilioaguiar/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/marcilioaguiar/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,6 +1,11 @@
 package br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +14,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.service.CandidateService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/candidate")
@@ -23,12 +29,21 @@ public class CandidateController {
     }
 
     @PostMapping("/signIn")
-    public String signIn(RedirectAttributes redirectAttributes, String username, String password) {
+    public String signIn(RedirectAttributes redirectAttributes, HttpSession session, String username, String password) {
 
         try {
             var token = this.candidateService.login(username, password);
+            var grants = token.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_"+role.toString().toUpperCase())).toList();
 
-            return "candidate/profile";
+            UsernamePasswordAuthenticationToken auth  = new UsernamePasswordAuthenticationToken(null, null, grants);
+            auth.setDetails(token);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+            session.setAttribute("token", token);
+
+            return "redirect:/candidate/profile";
 
         } catch (
 
@@ -39,6 +54,7 @@ public class CandidateController {
     }
 
     @GetMapping("/profile")
+    @PreAuthorize("hasRole('CANDIDATE')")
     public String profile() {
         return "candidate/profile";
     }

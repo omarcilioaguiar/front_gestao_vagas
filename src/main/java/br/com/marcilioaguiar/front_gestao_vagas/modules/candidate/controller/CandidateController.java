8e7faccf7ff132165e1e7f36883790e33cc8f5ef
Marcílio Aguiar <marcilio.paguiar@gmail.com>
2025-05-19 15:43:34 -0300

    Link Inscrever Backend

--- src/main/java/br/com/marcilioaguiar/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/marcilioaguiar/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,5 +1,7 @@
 package br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,9 +14,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.service.ApplyJobService;
 import br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.service.CandidateService;
 import br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.service.FindJobsService;
 import br.com.marcilioaguiar.front_gestao_vagas.modules.candidate.service.ProfileCandidateService;
@@ -32,6 +36,9 @@ public class CandidateController {
 
     @Autowired
     private FindJobsService findJobsService;
+
+    @Autowired
+    private ApplyJobService applyJobService;
 
     @GetMapping("/login")
     public String login() {
@@ -84,8 +91,6 @@ public class CandidateController {
     @PreAuthorize("hasRole('CANDIDATE')")
     public String jobs(Model model, String filter) {
 
-        System.out.println("O valor do filter é : " + filter);
-
         try {
             if (filter != null) {
         
@@ -97,6 +102,13 @@ public class CandidateController {
         }
 
         return "candidate/jobs";
+    }
+
+    @PostMapping("/jobs/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String applyForJob(@RequestParam("jobId") UUID jobId) {
+        this.applyJobService.execute(getToken(), jobId);
+        return "redirect:/candidate/jobs";
     }
 
     private String getToken() {
